5886dbd8861a78a7518cf90f374e56c21af4a508
Nico180803 <nicolascharpentier1808@gmail.com>
2025-04-04 12:18:03 +0200

    wip: user crud

--- src/Security/UtilisateurAuthenticator.php
+++ src/Security/UtilisateurAuthenticator.php
@@ -48,8 +48,8 @@ class UtilisateurAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
+
+        return new RedirectResponse($this->urlGenerator->generate('app_utilisateur_index'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
